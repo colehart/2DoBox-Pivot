d09ffbf9d0abd055a3516c7af80dcde7cf8203fd
Cole Hart <cole@colehart.com>
2018-07-25 16:44:48 -0600

    Finalize ARIA and style guide complatibility edits

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ function ListItem(title, body) {
   this.body = body;
   this.importance = 'Normal';
   this.complete = null;
-}
+};
 
 // ============================================================================
 //   Local Storage - Get and Set
@@ -40,8 +40,9 @@ function prependLocalStorage() {
   var currentCollection = parseLocalStorage();
   if (currentCollection) {
     var sortedCollection = currentCollection.filter(function(listItem) {
-      return listItem.complete === null
+      return listItem.complete === null;
     });
+
     printStorage(sortedCollection);
     checkForCompleted(currentCollection, sortedCollection);
   };
@@ -77,20 +78,23 @@ function prependCard(cardInfo, completed) {
       </div>
       <a aria-label="Mark task complete" class="complete-button js-complete-button" onclick="toggleComplete(event)">Complete</a>
     </div>
-  </article>`
+  </article>`;
 
   $('.js-bottom-box').prepend(listCard);
+  $('.js-title, .js-body').on('keydown', checkKey).on('blur', editCardText);
+  printComplete(completed);
+  hideExtras();
+};
 
+function printComplete(completed) {
+  if (completed) $('.card-container').first().toggleClass('complete');
+};
+
+function hideExtras() {
   if ($('article').length > 10) {
     $('article').slice(10).hide();
     $('.js-show-more').prop('disabled', '');
   };
-
-  if (completed) {
-    $('.card-container').first().toggleClass('complete');
-  };
-
-  $('.js-title, .js-body').on('keydown', checkKey).on('blur', editCardText);
 };
 
 // ============================================================================
@@ -105,18 +109,18 @@ function enableSave() {
 };
 
 function saveInputValues(event) {
-  event.preventDefault();
   var titleValue = $('.js-title-input').val().trim();
   var bodyValue = $('.js-body-input').val().trim();
+  event.preventDefault();
   newCard(titleValue, bodyValue);
-}
+};
 
 function newCard(titleValue, bodyValue) {
-    var listItem = new ListItem(titleValue, bodyValue);
-    prependCard(listItem);
-    addToLocalStorage(listItem);
-    resetForm();
-    enableFilter();
+  var listItem = new ListItem(titleValue, bodyValue);
+  prependCard(listItem);
+  addToLocalStorage(listItem);
+  resetForm();
+  enableFilter();
 };
 
 function resetForm() {
@@ -153,19 +157,19 @@ function checkKey(event) {
 };
 
 function trueEnter(event) {
-  if (event.which === 13 && event.shiftKey === false) {
+  if (event.which === 13 && !event.shiftKey) {
     $(event.target).blur();
   };
 };
 
 function editCardText(event) {
   if (!$(event.target).text()) {
-    alert('Please refresh the page and enter a title and description for your idea.');
+    alert('Please refresh the page and enter a title and a description for your idea.');
     return;
   } else {
-    $(event.target).text().trim();
     var card = $(event.target).closest('article');
     var cardId = card.prop('dataset').id;
+    $(event.target).text().trim();
     updateLocalStorage(card, cardId);
   };
 };
@@ -173,7 +177,7 @@ function editCardText(event) {
 function getCardQuality(event) {
   var card = $(event.target).closest('article');
   var cardId = card.prop('dataset').id;
-  var qualityHtml = card.find('.js-quality').text()
+  var qualityHtml = card.find('.js-quality').text();
   var possibleImport = ['None', 'Low', 'Normal', 'High', 'Critical'];
   var currIndex = jQuery.inArray(qualityHtml, possibleImport);
   setNewQuality(event, card, cardId, possibleImport, currIndex);
@@ -185,6 +189,7 @@ function setNewQuality(event, card, cardId, possibleImport, currIndex) {
   } else {
     downQuality(card, possibleImport, currIndex);
   };
+
   updateLocalStorage(card, cardId);
 };
 
@@ -213,6 +218,7 @@ function updateLocalStorage(card, cardId) {
       listItem.importance = card.find('.js-quality').text();
     };
   });
+
   stringifyNewCollection(currentCollection);
 };
 
@@ -234,9 +240,14 @@ function deleteListItem(event) {
 function removeFromCollection(deleteId) {
   var currentCollection = parseLocalStorage();
   var newCollection = currentCollection.filter(function(listItem) {
-     return listItem.id !== parseInt(deleteId);
+    return listItem.id !== parseInt(deleteId);
   });
-  !newCollection.length ? localStorage.clear() : stringifyNewCollection(newCollection);
+
+  if (!newCollection.length) {
+    localStorage.clear();
+  } else {
+    stringifyNewCollection(newCollection);
+  };
 };
 
 // ============================================================================
@@ -254,8 +265,16 @@ function filterCards() {
     var terms = $('.js-filter-input').val().toLowerCase();
     var title = $(this).find('.js-title').text().toLowerCase();
     var body = $(this).children('.js-body').text().toLowerCase();
-    title.includes(terms) || body.includes(terms) ? $(this).show() : $(this).hide();
+    checkContents(title, body, terms);
   });
+};
+
+function checkContents(title, body, terms) {
+  if (title.includes(terms) || body.includes(terms)) {
+    $(this).show();
+  } else {
+    $(this).hide();
+  };
 };
 
 function resetFilter() {
@@ -272,6 +291,7 @@ function toggleFilterButton(event) {
   var activeSiblings = $(event.target).siblings().filter(function(index) {
     return $(this).hasClass('active-filter');
   });
+
   $(event.target).toggleClass('active-filter');
   populateQualities(event, activeSiblings, buttonQualities);
 };
@@ -319,33 +339,35 @@ function cardFilter(cards, buttonQualities) {
 function toggleComplete(event) {
   var currentCollection = parseLocalStorage();
   var task = $(event.target).closest('article');
-  var taskID = task.prop('dataset').id
+  var taskId = task.prop('dataset').id;
   task.toggleClass('complete');
-  setComplete(event, task, taskID, currentCollection)
+  setComplete(event, task, taskId, currentCollection);
 };
 
-function setComplete(event, task, taskID, currentCollection) {
+function setComplete(event, task, taskId, currentCollection) {
   if (task.prop('className') === 'card-container complete') {
-     storeCompleteProp(taskID, currentCollection);
-    } else if (task.prop('className') === 'card-container') {
-     storeIncomplete(taskID, currentCollection);
-    };
+    storeCompleteProp(taskId, currentCollection);
+  } else if (task.prop('className') === 'card-container') {
+    storeIncomplete(taskId, currentCollection);
   };
+};
 
-function storeCompleteProp(taskID, currentCollection) {
+function storeCompleteProp(taskId, currentCollection) {
   currentCollection.forEach(function(listItem) {
-    if (listItem.id === parseInt(taskID)) {
+    if (listItem.id === parseInt(taskId)) {
       listItem.complete = true;
     };
+
     stringifyNewCollection(currentCollection);
   });
 };
 
-function storeIncomplete(taskID, currentCollection) {
+function storeIncomplete(taskId, currentCollection) {
   currentCollection.forEach(function(listItem) {
-    if (listItem.id === parseInt(taskID)) {
+    if (listItem.id === parseInt(taskId)) {
       listItem.complete = null;
     };
+
     stringifyNewCollection(currentCollection);
   });
 };
@@ -355,8 +377,8 @@ function storeIncomplete(taskID, currentCollection) {
 // ============================================================================
 
 function showComplete(event) {
-  event.preventDefault();
   var currentCollection = parseLocalStorage();
+  event.preventDefault();
   prependComplete(currentCollection);
   disableCompleteTaskBtn();
 };
@@ -366,6 +388,7 @@ function prependComplete(collection) {
     var completedTasks = collection.filter(function(listItem) {
      return listItem.complete === true;
     });
+
     completedTasks.forEach(function(listItem) {
       prependCard(listItem, true);
     });
